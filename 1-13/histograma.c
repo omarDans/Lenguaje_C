167f0f6b70de7ae55c3a1284404d11ea71bb78d3
omarDans <omardanscastro@gmail.com>
2024-09-21 19:21:29 +0200

    added 1-13

--- 1-13/histograma.c
+++ 1-13/histograma.c
@@ -1,34 +1,96 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define OUT 0
 #define IN 1
 
-void main()
+
+void print_vertical(char **buffer, int verSize, int horSize) {
+    int x, y;
+
+    // Imprimir las palabras en columnas
+    for (x = 0; x < verSize; x++) {
+        for (y = 0; y < horSize; y++) { 
+            if (x < strlen(buffer[y])) { 
+                printf("%c", buffer[y][x]);  
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n"); 
+    }
+}
+
+int main()
 {
-    char buffer[100];
-    int nc = 0;
+    int nc, nw, bw; // number_character, new_word, biggest_word
+    char **buffer = NULL;
+    nc = nw = 0;
     int i = 0;
     char c;
     int state = OUT;
+
+    int capacidad_palabras = 10;
+    int tamaño_palabras = 100;
+
+    buffer = (char**)malloc(capacidad_palabras * sizeof(char *));
+    if (buffer == NULL) {
+        printf("Error asignando memoria\n");
+        return 1;
+    }
+
+    buffer[nw] = (char *)malloc(tamaño_palabras * sizeof(char));
+    if (buffer[nw] == NULL){
+        printf("Error asignando memoria\n");
+        return 2;
+    }
+
     while ((c = getchar()) != EOF) {
         if (c == ' ' || c == '\n' || c == '\t'){
             if (state == IN) {
-                buffer[nc] = '\n';
                 state = OUT;
-                nc++;
+                nw++;
+                if (nc > bw) {
+                    bw = nc;
+                }
+                nc = 0;
+
+                if (nw > capacidad_palabras) {
+                    capacidad_palabras *= 2;
+                    buffer = (char**)realloc(buffer, capacidad_palabras * sizeof(char *));
+                    if (buffer == NULL) {
+                        printf("Error al redimensionar\n");
+                        return 3;
+                    }
+                    printf("memoria añadida\n");
+                }
+
+                buffer[nw] = (char *)malloc(tamaño_palabras * sizeof(char));
+                if (buffer[nw] == NULL) {
+                    printf("Error asignando memoria para la palabra %d\n", nw);
+                    return 4;
+                }
             }
+            
         } else {
             if (state == OUT) {
                 state = IN;
             }
-            buffer[nc] = '0';
+            buffer[nw][nc] = '0';
             nc++;
         }
         
     }
-    buffer[nc] = '\0';
+    printf("VERTICAL\n\n");
+    print_vertical(buffer, bw, nw);
+    
+    printf("\n\nHORIZONTAL\n\n");
+    for (i = 0; i < nw; i++) {
+        printf("%s\n", buffer[i]);
+        free(buffer[i]);
+    }
 
-    printf("HISTOGRAMA\n\n%s\n", buffer);
-
-}
+    free(buffer);
+    return 0;
+}    
