31aeee14c55d90b1ecf0f7d716de90d5d9b9c97c
omarDans <omardanscastro@gmail.com>
2024-09-21 14:13:31 +0200

    added 1-10

---
+++ 1-10/replaceChars.c
@@ -0,0 +1,28 @@
+// No encontré otra manera de hacer esto sin usar la libreria ncurses ya que getchar() no registra el retroceso
+
+#include <stdio.h>
+#include <ncurses.h>
+
+int main(void) {
+    initscr();             
+
+    int c;
+    printw("Escribe algo (Ctrl+D para salir): ");
+    refresh();
+
+    while ((c = getch()) != 4) { 
+        if (c == '\t') {
+            printf("\\t");      
+        } else if (c == 127) {
+            printf("\\b");        
+        } else if (c == 10 || c == KEY_ENTER) {
+            printf("\\n");        
+        } else if (c == '\\') {
+            printf("\\\\");       
+        } else {
+            putchar(c);           
+        }
+    }
+    endwin();  
+    return 0;
+}
